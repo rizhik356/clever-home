2c9f9c0f409417f56dc95298f464f2e051957e79
makhanek <makhanek@infocom.dev>
2024-10-01 12:26:49 +0300

    add badge

--- src/ui/UserDropDown/UserDropDown.tsx
+++ src/ui/UserDropDown/UserDropDown.tsx
@@ -1,36 +1,13 @@
 import { Avatar, Badge, Dropdown } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
 import style from './scss/style.module.scss'
-import { CiSettings } from 'react-icons/ci'
-import { CgProfile } from 'react-icons/cg'
-import { IoExitOutline } from 'react-icons/io5'
-
-const items = [
-  {
-    label: 'Настройки',
-    key: 'setting',
-    icon: <CiSettings size={20} />,
-  },
-  {
-    label: 'Профиль',
-    key: 'profile',
-    icon: <CgProfile size={18} />,
-  },
-  {
-    type: 'divider',
-  },
-  {
-    label: 'Выйти',
-    key: 'exit',
-    icon: <IoExitOutline size={20} />,
-  },
-]
+import items from './sources/dropdownItems'
 
 const UserDropDown = () => {
   return (
-    <Dropdown menu={{ items }} trigger={['click']}>
+    <Dropdown menu={{items}} trigger={['click']}>
       <div onClick={(e) => e.preventDefault()}>
-        <Badge>
+        <Badge count={12}>
           <Avatar
             className={style['avatar']}
             size="large"
---
+++ src/ui/UserDropDown/sources/dropDownItems.tsx
@@ -0,0 +1,33 @@
+import { CiSettings } from 'react-icons/ci'
+import { CgProfile } from 'react-icons/cg'
+import { IoExitOutline } from 'react-icons/io5'
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { MenuProps } from 'antd';
+
+const dropDownItems: MenuProps['items'] = [
+    {
+        label: 'Настройки',
+        key: 'setting',
+        icon: <CiSettings size={20} />,
+      },
+      {
+        label: 'Профиль',
+        key: 'profile',
+        icon: <CgProfile size={18} />,
+      },
+      {
+        label: 'Уведомления',
+        key: 'notifications',
+        icon: <IoIosNotificationsOutline size={20} />,
+      },
+      {
+        type: 'divider',
+      },
+      {
+        label: 'Выйти',
+        key: 'exit',
+        icon: <IoExitOutline size={20} />,
+      },
+]
+
+export default dropDownItems
