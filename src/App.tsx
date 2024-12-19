ad180bdf49e9b0cbc31eb975d6014152c1dfd7ce
makhanek <makhanek@infocom.dev>
2024-12-19 13:18:20 +0300

    app chng

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('access_token')
 
     if (token) {
       dispatch(setToken(token))
